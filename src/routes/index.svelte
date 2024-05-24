2437dcc89ab2fc285bee6a289d9336a2ab786a72
Andrew Gilgallon <andrew@variancestudios.com>
2024-05-23 22:48:19 -0400

    No max w on mobile

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -54,7 +54,7 @@
   
   <svelte:window on:keydown={keydown} on:pointerdown={keydown} />
   
-  <div class='select-none absolute text-center left-1/2 top-1/2 text-5xl md:text-6xl -translate-y-1/2 -translate-x-1/2 px-2 max-w-lg'>
+  <div class='select-none absolute text-center left-1/2 top-1/2 text-5xl md:text-6xl -translate-y-1/2 -translate-x-1/2 px-2 md:max-w-lg'>
     {currentWord[turkish ? 0 : 1]}
   </div>
 
