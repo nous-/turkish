a097cd3089f73f74886791fac9d28a6c3862988d
Andrew Gilgallon <andrew@variancestudios.com>
2024-05-23 21:38:53 -0400

    Initial checkin

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: 'class',
+    content: ['./src/**/*.svelte', './src/**/*.css', './index.html'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
